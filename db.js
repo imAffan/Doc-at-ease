672caf8346bb5cbc7e7929f01ad1a6db8cf152dd
Affan Ahmed <105587806+imAffan@users.noreply.github.com>
2023-03-07 22:49:21 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,38 @@
+const { MongoClient, ServerApiVersion, ObjectID } = require('mongodb');
+require('dotenv').config()
+const uri = "mongodb+srv://sahithkocherla:abcdef1234@cluster0.aj9bc3p.mongodb.net/tests";
+
+
+let _db;
+
+const initDb = callback => {
+    if (_db) {
+        console.log("Database is already initialized")
+        return callback(null, _db);
+    }
+
+    MongoClient.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: ServerApiVersion.v1,
+    })
+        .then(client => {
+            _db = client;
+            callback(null, _db)
+        })
+        .catch(err => {
+            callback(err)
+        })
+}
+
+const getDb = () => {
+    if (!_db) {
+        throw Error('Database not initialized')
+    }
+    return _db
+}
+
+module.exports={
+    getDb,
+    initDb
+}
